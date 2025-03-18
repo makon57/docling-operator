39ebcc5c9f81109bc014f291fa31488dc2e72d37
Manna Kong <mkong@redhat.com>
2025-03-18 15:30:01 -0500

    Add Service subreconciler
    Signed-off-by: Manna Kong <mkong@redhat.com>

--- internal/reconcilers/service.go
+++ internal/reconcilers/service.go
@@ -5,6 +5,10 @@ import (
 
 	"github.com/go-logr/logr"
 	"github.io/opdev/docling-operator/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -25,5 +29,40 @@ func NewServiceReconciler(client client.Client, log logr.Logger, scheme *runtime
 
 func (r *ServiceReconciler) Reconcile(ctx context.Context, doclingServ *v1alpha1.DoclingServ) (bool, error) {
 
+	service := &corev1.Service{}
+	err := r.Get(ctx, types.NamespacedName{Name: doclingServ.Name, Namespace: doclingServ.Namespace}, service)
+	if err != nil && errors.IsNotFound(err) {
+		r.Log.Info("Creating a new Service", "Service", doclingServ.Name)
+		svc := r.createService(doclingServ)
+
+		err = r.Create(ctx, svc)
+		if err != nil {
+			r.Log.Error(err, "Failed to create new Service", "Service.Namespace", service.Namespace, "Service.Name", service.Name)
+			return true, err
+		}
+	}
+
 	return false, nil
+}
+
+func (r *ServiceReconciler) createService(doclingServ *v1alpha1.DoclingServ) *corev1.Service {
+
+	service := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:		doclingServ.Name,
+			Namespace:  doclingServ.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Port: 5001,
+					Protocol: corev1.ProtocolTCP,
+				},
+			},
+			Selector: map[string]string{
+				"app": "docling-serve",
+			},
+		},
+	}
+	return service
 }
